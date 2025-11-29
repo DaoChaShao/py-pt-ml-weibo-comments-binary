9b197724545f487fd341a1d13182acb0ff588b67
DaoChaShao <27882272@qq.com>
2025-11-30 01:01:13 +0800

    feat: add new file paths for data sources in cfg_base.py

--- src/configs/cfg_base.py
+++ src/configs/cfg_base.py
@@ -15,13 +15,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 @dataclass
 class FilePaths:
     API_KEY: Path = BASE_DIR / "data/api_keys.yaml"
+    DATA4ALL: Path = BASE_DIR / "data/weibo_comments.csv"
+    DATA4TRAIN: Path = BASE_DIR / "data/train/"
+    DATA4TEST: Path = BASE_DIR / "data/test/"
     DICTIONARY: Path = BASE_DIR / "data/dictionary.json"
     LOGS: Path = BASE_DIR / "logs/"
     SAVED_NET: Path = BASE_DIR / "models/model.pth"
     SPACY_MODEL_EN: Path = BASE_DIR / "models/spacy/en_core_web_md"
     SPACY_MODEL_ZH: Path = BASE_DIR / "models/spacy/zh_core_web_md"
-    TRAIN_DATA: Path = BASE_DIR / "data/train/"
-    TEST_DATA: Path = BASE_DIR / "data/test/"
 
 
 @dataclass
