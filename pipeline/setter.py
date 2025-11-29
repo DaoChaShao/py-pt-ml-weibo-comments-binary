4c57ce0b3c943ea1b9328b8c47c5065954b59603
DaoChaShao <27882272@qq.com>
2025-11-29 20:34:22 +0800

    feat: add setter.py for data preparation functionality

---
+++ pipeline/setter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/11/29 20:21
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   setter.py
+# @Desc     :
+
+from pipeline import process_data
+
+
+def prepare_data() -> None:
+    """ Main Function """
+    pass
+
+
+if __name__ == "__main__":
+    pass
