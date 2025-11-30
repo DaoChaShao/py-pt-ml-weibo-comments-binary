3e9f6c74603f90e2d71a88b3e421e5cad55da6ae
DaoChaShao <27882272@qq.com>
2025-11-30 23:00:48 +0800

    feat: implement main function for RNN sequence classification with argument parsing and model training

--- main.py
+++ main.py
@@ -7,29 +7,68 @@
 # @Desc     :   
 
 from argparse import ArgumentParser
+from sys import argv
+from torch import nn, optim
 
 from pipeline import prepare_data
+from src.configs.cfg_rnn import CONFIG4RNN
+from src.trainers.rnn_seq_classification import TorchTrainer4Seq2Classification
+from src.nets.rnn_lstm_classification import LSTMRNNForClassification
+from src.utils.helper import Beautifier
+from src.utils.PT import TorchRandomSeed
 
 
 def main() -> None:
     """ Main Function """
-    pass
+    # Set up argument parser
+    parser = ArgumentParser()
+    parser.add_argument("-a", "--alpha", type=float, default=1e-4, help="Learning rate")
+    parser.add_argument("-e", "--epochs", type=int, default=100, help="Number of training times")
+    args = parser.parse_args()
+    """
+    Display hyperparameters You need to set before training using command lines:
+    1.Running: python main.py -h
+    2.Or running: python main.py --help
+    """
 
+    with TorchRandomSeed("Weibo Seq Classification"):
+        # Prepare data
+        train_loader, valid_loader, dictionary = prepare_data()
 
-"""
-import argparse
-from src.configs import trainer_config, dataset_config
-from src.nets.unet.standard4 import Standard4LayersUNet
-from src.datasets.unet.binary_dataset import BinarySegDataset
-from src.trainers.unet.binary_trainer import UNetSegmentationTrainer
+        # Get the input size and number of classes
+        vocab_size: int = len(dictionary)
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--batch_size", type=int, default=trainer_config.TRAINER_CONFIG["batch_size"])
-parser.add_argument("--lr", type=float, default=trainer_config.TRAINER_CONFIG["learning_rate"])
-parser.add_argument("--device", type=str, default=trainer_config.TRAINER_CONFIG["device"])
-parser.add_argument("--model_save_path", type=str, default="./logs/unet_binary/best_model.pth")
-args = parser.parse_args()
-"""
+        # Initialize model
+        model = LSTMRNNForClassification(
+            vocab_size=vocab_size,
+            embedding_dim=CONFIG4RNN.PARAMETERS.EMBEDDING_DIM,
+            hidden_size=CONFIG4RNN.PARAMETERS.HIDDEN_SIZE,
+            num_layers=CONFIG4RNN.PARAMETERS.LAYERS,
+            num_classes=CONFIG4RNN.PARAMETERS.CLASSES,
+            dropout_rate=CONFIG4RNN.PREPROCESSOR.DROPOUT,
+        )
+        # Setup optimizer and loss function
+        optimizer = optim.AdamW(model.parameters(), lr=args.alpha, weight_decay=CONFIG4RNN.HYPERPARAMETERS.DECAY)
+        scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode="min", factor=0.5, patience=2)
+        criterion = nn.CrossEntropyLoss()
+        model.summary()
+
+        # Setup trainer
+        trainer = TorchTrainer4Seq2Classification(
+            model=model,
+            optimiser=optimizer,
+            criterion=criterion,
+            scheduler=scheduler,
+            accelerator=CONFIG4RNN.HYPERPARAMETERS.ACCELERATOR,
+        )
+        # Train the model
+        trainer.fit(
+            train_loader=train_loader,
+            valid_loader=valid_loader,
+            epochs=args.epochs,
+            model_save_path=str(CONFIG4RNN.FILEPATHS.SAVED_NET)
+        )
+
 
 if __name__ == "__main__":
     main()
